29797481d6cfe4ed63494805a86b47779ebf0283
Serhii Pylypchuk <sergiydjan@gmail.com>
2020-05-26 16:31:40 +0300

    Fix no env issue

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import models from './models'
 
-const uri = process.env.MONGOLAB_URI || '';
+const uri = process.env.MONGOLAB_URI || 'mongodb+srv://test_unknown:123qwe@mytestdb-shqsn.mongodb.net/test?retryWrites=true&w=majority';
 
 export async function connectDb() {
     await mongoose.connect(uri,  { useNewUrlParser: true });
